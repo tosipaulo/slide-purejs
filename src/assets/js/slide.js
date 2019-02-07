edd7296c4caca1044d5ed6509e2113b10a3c81ce
Paulo Tosi <tosi.paulo@gmail.com>
2019-02-07 21:39:15 -0200

    Adicionando eventos touch

--- src/assets/js/slide.js
+++ src/assets/js/slide.js
@@ -3,24 +3,50 @@ export default class Slide {
     constructor(slide, wraper) {
         this.slide = document.querySelector(slide);
         this.wraper = document.querySelector(wraper);
+        this.dist = { finalPosition: 0, startX: 0, movement: 0, movePosition: 0}
+    }
+
+    moveSlide(distX) {
+        this.dist.movePosition = distX;
+        this.slide.style.transform = `translate3d(${distX}px, 0px, 0px)`
+    }
+
+    updatePosition(clientX) {
+        this.dist.movement = (this.dist.startX - clientX) * 1.6;
+        return this.dist.finalPosition - this.dist.movement;
     }
 
     onStart(event) {
-        event.preventDefault();
-        this.wraper.addEventListener('mousemove', this.onMove);
+        let moveType;
+        if(event.type == 'mousedown') {
+            event.preventDefault();
+            this.dist.startX = event.clientX;
+            moveType = 'mousemove';
+        }else{
+            moveType = 'touchmove';
+            this.dist.startX = event.changedTouches[0].clientX
+        }
+        
+        this.wraper.addEventListener(moveType, this.onMove);
     }
 
     onMove(event) {
-        console.log("moveu");
+        const pointerPosition = (event.type == "mousemove") ? event.clientX : event.changedTouches[0].clientX
+        const finalPosition = this.updatePosition(pointerPosition);
+        this.moveSlide(finalPosition);
     }
 
     onEnd(event) {
-        this.wraper.removeEventListener('mousemove', this.onMove);
+        const moveType = (event.type == 'mouseup') ? 'mousemove' : 'touchmove'
+        this.wraper.removeEventListener(moveType, this.onMove);
+        this.dist.finalPosition = this.dist.movePosition;
     }
 
     addSlideEvents() {
         this.wraper.addEventListener('mousedown', this.onStart);
+        this.wraper.addEventListener('touchstart', this.onStart);
         this.wraper.addEventListener('mouseup', this.onEnd);
+        this.wraper.addEventListener('touchend', this.onEnd);
     }
 
     bindEvents() {
