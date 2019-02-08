ad4ba6f91d42f011c39e2a7d94a74264c0d57bbb
Paulo Tosi <tosi.paulo@gmail.com>
2019-02-07 22:13:19 -0200

    Criando configurações do index slides

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -3,5 +3,6 @@ import Slide from './slide';
 
 const slide = new Slide('.slide', '.slide-wraper');
 slide.init();
+slide.changeSlide(1);
 
 console.log(slide) 
--- src/assets/js/slide.js
+++ src/assets/js/slide.js
@@ -55,7 +55,41 @@ export default class Slide {
         this.onEnd = this.onEnd.bind(this);
     }
 
+    //Slides config
+
+    slidePosition(slide) {
+        const margin = (this.wraper.offsetWidth - slide.offsetWidth) / 2;
+        return -(slide.offsetLeft - margin)
+    }
+
+    slidesConfig() {
+        this.slideArray = [...this.slide.children].map(element => {
+            const position = this.slidePosition(element)
+            return {
+                position,
+                element
+            }
+        });
+    }
+
+    slidesIndexNav(index) {
+        const last = this.slideArray.length;
+        this.index = {
+            prev: index ? index - 1 : undefined,
+            active: index,
+            next: index === last ? undefined : index + 1
+        }
+    }
+
+    changeSlide(index) {;
+        const activeSlide = this.slideArray[index]
+        this.moveSlide(activeSlide.position);
+        this.slidesIndexNav(index);
+        this.dist.finalPosition = activeSlide.position
+    }
+
     init() {
+        this.slidesConfig();
         this.bindEvents();
         this.addSlideEvents();
         return this
